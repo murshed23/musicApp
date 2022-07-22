6425e83ff9c07debe26846299aac297e9c66e28b
Murshed Ahmed <murshed4400@gmail.com>
2022-07-22 20:13:18 +0530

    db set

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
 const mysql = require('mysql');
 
@@ -9,7 +11,19 @@ const db = mysql.createPool({
   database: 'spotify-like'
 });
 
-app.get("/", (req, res) => {});
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.post("/insert", (req, res) => {
+
+  const songName = req.body.songName
+
+  const query = "INSERT INTO songs (name) VALUES (?)";
+  db.query(query, [songName], (err, result) => {
+    console.log(result);
+  });
+});
 
 app.listen(3001, () => {
     console.log("Runnin... ");
